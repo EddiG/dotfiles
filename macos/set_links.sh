d7b1f412991c76dd47ab62d5a6b338721c334719
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2019-02-14 00:59:37 +0500

    Add script for setting links

---
+++ macos/set_links.sh
@@ -0,0 +1,8 @@
+PWD=`pwd`
+
+ln -s $PWD/vimrc ~/.config/nvim/init.vim
+ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -s $PWD/bash_profile ~/.bash_profile
+ln -s $PWD/bash_prompt ~/.bash_prompt
+ln -s $PWD/inputrc ~/.inputrc
+ln -s $PWD/gitconfig ~/.gitconfig

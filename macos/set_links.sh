b989eb69be7d10c869deb2172811c4f85a37c96f
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2020-05-26 02:06:59 +0500

    Add ZSH configs

--- macos/set_links.sh
+++ macos/set_links.sh
@@ -1,9 +1,12 @@
 PWD=`pwd`
 
 ln -s $PWD/vimrc ~/.config/nvim/init.vim
-ln -s $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
+# ln -s $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/bash_prompt ~/.bash_prompt
+ln -s $PWD/zprofile ~/.zprofile
+ln -s $PWD/zshrc ~/.zshrc
+ln -s $PWD/p10k.zsh ~/.p10k.zsh
 ln -s $PWD/inputrc ~/.inputrc
 ln -s $PWD/gitconfig ~/.gitconfig

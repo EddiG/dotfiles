d6a67460cc5a33622583a530309681431afa8ef9
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2024-02-14 23:38:10 +0500

    Add theme for bat

--- macos/set_links.sh
+++ macos/set_links.sh
@@ -7,3 +7,4 @@ ln -s $PWD/tmux.conf ~/.config/tmux/tmux.conf
 ln -s $PWD/tmux.tokyonight.conf ~/.config/tmux/tmux.tokyonight.conf
 ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/nnn_preview_cmd.sh ~/nnn_preview_cmd.sh
+ln -s $PWD/bat ~/.config/bat
